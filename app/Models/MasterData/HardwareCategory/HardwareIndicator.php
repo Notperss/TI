ed900af97a58226c8a6290a873e82484fe57dbda
Notperss <Adhityanugroho70@gmail.com>
2025-03-26 09:39:22 +0700

    update hardware category

--- app/Models/MasterData/HardwareCategory/HardwareIndicator.php
+++ app/Models/MasterData/HardwareCategory/HardwareIndicator.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\MasterData\HardwareCategory;
 
+use App\Models\Inspection\InspectionIndicatorAsset;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -18,5 +19,9 @@ class HardwareIndicator extends Model
     public function hardwareCategory()
     {
         return $this->belongsTo(HardwareCategory::class, 'hardware_category_id');
+    }
+    public function inspectionAssets()
+    {
+        return $this->hasMany(InspectionIndicatorAsset::class);
     }
 }
